c675068f04ba23d2a506ed04f55e610c47c3fb06
DhanoHacks <96428099+DhanoHacks@users.noreply.github.com>
2021-12-20 23:47:20 +0530

    Update scriptblue.py

--- scriptblue.py
+++ scriptblue.py
@@ -7,4 +7,12 @@ def ActRobot(robot):
         
         return randint(1,4)
 
+def ActBase(base):
+    '''
+    Add your code here
+    
+    '''
+    if base.GetElixir() > 500:
+            base.create_robot('')
 
+    return
